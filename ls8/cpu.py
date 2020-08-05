ca30f61b0c86b76e7418c2c4e20d5c75cda93cdd
MicahJank <MicahJank@users.noreply.github.com>
2020-08-05 14:13:27 -0500

    think i might be understanding it now

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -71,4 +71,8 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        IR = self.ram[self.pc]
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+
+        
