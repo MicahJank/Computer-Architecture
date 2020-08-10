817bd924bda210fc4bc19cef52b71a0d46538ccc
Micah <micahjank@gmail.com>
2020-08-10 11:38:35 -0500

    stuff

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,9 +7,14 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        # pc is the program counter and keeps track of where we are in the 
         self.pc = 0
-        self.ram = [0x00] * 0xFF
-        self.reg = [0] * 8
+        self.ram = [0x00] * 0xFF # <-- stores 256 bytes
+
+        self.reg = [0] * 8 # <-- total of 8 registers
+
+        # branchtable provides O(1) access to handler functions - 
+        # prevents us from having to check the opcode value against EVERY possible function - (O(n) time complexity)
         self.branchtable = {
             0b00000001: self.HLT,
             0b10000010: self.LDI,
@@ -17,24 +22,33 @@ class CPU:
             0b10100010: self.MUL
         }
     
+    # handler functions to store inside the branchtable #
+    # HLT exits the program regardless of what is happening
     def HLT(self):
         sys.exit()
 
+    # takes a value and stores it inside a register
     def LDI(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
+        operand_a = self.ram_read(self.pc + 1) # the register number
+        operand_b = self.ram_read(self.pc + 2) # the value to store
         self.reg[operand_a] = operand_b
     
+    # Prints the value located at the register
     def PRN(self):
-        operand_a = self.ram_read(self.pc + 1)
+        operand_a = self.ram_read(self.pc + 1) # the register number
         print(self.reg[operand_a])
 
+    # Multiplies to register values together and assigns the result to a register
     def MUL(self, register_a, register_b):
         self.reg[register_a] = self.reg[register_a] * self.reg[register_b]
 
+    #                       #                     #
+
+    # returns a location in the ram based on the address passed to it
     def ram_read(self, address):
         return self.ram[address]
     
+    # sets a value to the location in ram based on the address and value passed to it
     def ram_write(self, value, address):
         self.ram[address] = value
 
@@ -45,33 +59,45 @@ class CPU:
 
         address = 0
 
+        # the input from the user should be of length 2 or more if it was entered correctly
         if len(sys.argv) < 2:
             print("No path to file given.")
             print("e.g - filename path/to/file")
             sys.exit()
 
         try:
+            # Open the file
             with open(sys.argv[1]) as file:
+                # loop over the lines in the file
                 for line in file:
+                    # return a list from the lines in the file wherever a comment # appears
                     comment_split = line.split('#')
 
+                    # grab the first item in the list
                     possible_num = comment_split[0]
 
+                    # if it is empty we can skip
                     if possible_num == '':
                         continue
-                
+                    
+                    # next check to see if the first char in the string is a 1 or 0
                     if possible_num[0] == "1" or possible_num[0] == "0":
+                        # if it is we know the length of each opcode should be 8 so we slice to the 8th index
                         num = possible_num[:8]
+                        # this will store the opcode/num in the ram at the address location
                         self.ram[address] = int(num, 2)
+
+                        # increase the address
                         address += 1
         except FileNotFoundError:
             print(f'{sys.argv[0]}: {sys.argv[1]} not found')
 
 
+    # this runs for certain functions that the alu should handle
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
         try:
-            self.branchtable[op](reg_a, reg_b)
+            self.branchtable[op](reg_a, reg_b) # <-- these functions take in arguments
         # if op == "ADD":
         #     self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
@@ -98,76 +124,6 @@ class CPU:
 
         print()
 
-    # takes a binary opcode and returns a string representation of the instructions (i.e - 0b00000001 would give back 'HLT')
-    def decodeInstruction(self, opcode):
-        if opcode == 0b00000001:
-            return 'HLT'
-        elif opcode == 0b10000010:
-            return 'LDI'
-        elif opcode == 0b01000111:
-            return 'PRN'
-        elif opcode == 0b10100000:
-            return 'ADD'
-        elif opcode == 0b10101000:
-            return 'AND'
-        elif opcode == 0b01010000:
-            return 'CALL'
-        elif opcode == 0b10100111:
-            return 'CMP'
-        elif opcode == 0b01100110:
-            return 'DEC'
-        elif opcode == 0b10100011:
-            return 'DIV'
-        elif opcode == 0b01100101:
-            return 'INC'
-        elif opcode == 0b01010010:
-            return 'INT'
-        elif opcode == 0b00010011:
-            return 'IRET'
-        elif opcode == 0b01010101:
-            return 'JEQ'
-        elif opcode == 0b01011010:
-            return 'JGE'
-        elif opcode == 0b01010111:
-            return 'JGT'
-        elif opcode == 0b01011001: 
-            return 'JLE'
-        elif opcode == 0b01011000:
-            return 'JLT'
-        elif opcode == 0b01010100:
-            return 'JMP'
-        elif opcode == 0b01010110:
-            return 'JNE'
-        elif opcode == 0b10000011:
-            return 'LD'
-        elif opcode == 0b10100100:
-            return 'MOD'
-        elif opcode == 0b10100010:
-            return 'MUL'
-        elif opcode == 0b00000000:
-            return 'NOP'
-        elif opcode == 0b01101001:
-            return 'NOT'
-        elif opcode == 0b10101010:
-            return 'OR'
-        elif opcode == 0b01000110:
-            return 'POP'
-        elif opcode == 0b01001000:
-            return 'PRA'
-        elif opcode == 0b01000101:
-            return 'PUSH'
-        elif opcode == 0b00010001:
-            return 'RET'
-        elif opcode == 0b10101100:
-            return 'SHL'
-        elif opcode == 0b10101101:
-            return 'SHR'
-        elif opcode == 0b10000100:
-            return 'ST'
-        elif opcode == 0b10100001:
-            return 'SUB'
-        elif opcode == 0b10101011:
-            return 'XOR'
     def run(self):
         """Run the CPU."""
         # format of opcode is AABCDDDD
@@ -176,12 +132,16 @@ class CPU:
         # C - 1 if this instruction sets the PC
         # DDDD - Instruction identifier
         
-
+        # the while loop will run through all the instructions that need to be ran using the pc as a guide for where it is
         while self.pc <= len(self.ram):
-            IR = self.ram[self.pc]
-            num_of_ops = IR >> 6
-            is_alu_op = IR >> 5 & 0b001 # if 0 false if 1 true
-            pc_set = IR >> 4 & 0b0001 # 1 if instruction sets pc 0 if it doesnt
+            IR = self.ram[self.pc] # gets the instruction from the location in the ram
+            # bitshift the instruction to the left 6 this will leave me with just the 2 digits left and from that i can
+            # find out how many operands will be needed in combinartion with the initial one
+            num_of_ops = IR >> 6 
+            
+            is_alu_op = IR >> 5 & 0b001 # 0 if false, 1 if true
+            pc_set = IR >> 4 & 0b0001 # 1 if instruction sets pc, 0 if it doesnt
+
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
@@ -190,7 +150,8 @@ class CPU:
             if is_alu_op:
                 self.alu(IR, operand_a, operand_b)
             else:
-                self.branchtable[IR]()
+                # What is stored in the branchatable are functions which is why we can invoke this
+                self.branchtable[IR]() 
 
             # pc needs to increment by 1 (for the current operation) + however many extra operands there will be
             self.pc += 1 + num_of_ops
