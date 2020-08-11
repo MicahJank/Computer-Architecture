bc82d80bb1cf74d01a4a39413db2a644c0f3e221
MicahJank <MicahJank@users.noreply.github.com>
2020-08-11 12:41:34 -0500

    MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,13 +13,18 @@ class CPU:
 
         self.reg = [0] * 8 # <-- total of 8 registers
 
+        # register 7 is reserved for the stack pointer and the initial place where the SP points is at F4
+        self.reg[7] = 0xF4
+
         # branchtable provides O(1) access to handler functions - 
         # prevents us from having to check the opcode value against EVERY possible function - (O(n) time complexity)
         self.branchtable = {
             0b00000001: self.HLT,
             0b10000010: self.LDI,
             0b01000111: self.PRN,
-            0b10100010: self.MUL
+            0b10100010: self.MUL,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP
         }
     
     # handler functions to store inside the branchtable #
@@ -42,6 +47,20 @@ class CPU:
     def MUL(self, register_a, register_b):
         self.reg[register_a] = self.reg[register_a] * self.reg[register_b]
 
+    # pushes the value of the given register to the stack
+    def PUSH(self):
+        reg_num = self.ram_read(self.pc + 1)
+        self.reg[7] -= 1
+        SP = self.reg[7]
+        value = self.reg[reg_num]
+
+        self.ram_write(value, SP)
+
+    def POP(self):
+        reg_num = self.ram_read(self.pc + 1)
+        SP = self.reg[7]
+        self.reg[reg_num] = self.ram_read(SP)
+        self.reg[7] += 1 
     #                       #                     #
 
     # returns a location in the ram based on the address passed to it
